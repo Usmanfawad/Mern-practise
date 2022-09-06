cd0646dec66b54ae0f0cabf12cf80d92d92a85d7
Usman Fawad <ufawad0@gmail.com>
2022-09-06 13:49:43 +0500

    App structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <ul className='goal-list'>
+        <li>Finish the course</li>
+        <li>Learning the course</li>
+        <li>Gonna be my chammak challo</li>
+      </ul>
     </div>
+
   );
 }
 
